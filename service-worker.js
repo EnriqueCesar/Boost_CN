b3780e42ef78b4898dd55ce5db2e291ef232e660
EnriqueCesar <ecesar.alsea01@gmail.com>
2026-07-14 12:36:55 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="boost-cn-v7-20260714";
+const CACHE="boost-cn-v8-20260714";
 const CORE=["./","./index.html","./css/styles.css","./js/app.js","./manifest.json","./assets/boost-cn.png","./data/manifest-data.json","./data/base-at.json","./data/item-adt.json","./data/records-001.json","./data/records-002.json","./data/records-003.json","./data/records-004.json","./data/records-005.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
